8bb7bd44bd50b833df0008b14c5ed85a717620e3
schollsebastian <scholl.sebastian@gmx.at>
2021-01-26 17:50:28 +0100

    Clean up index.js

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('Gonk!', { type: 'PLAYING' });
+    client.user.setActivity('Gonk!', { type: 'PLAYING' })
+        .catch(console.error);
     console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on('message', (message) => {
-    if (
-        message.author.id !== client.user.id && (
-        Math.floor(Math.random() * 30) === 0
-        || message.content.toLowerCase().includes('gonk'))
-        ) {
-        message.channel.send('Gonk!');
+    let authorIsNotClient = message.author.id !== client.user.id;
+    let randomMessage = Math.floor(Math.random() * 30) === 0;
+    let messageIncludesGonk = message.content.toLowerCase().includes('gonk');
+
+    if (authorIsNotClient && (randomMessage || messageIncludesGonk)) {
+        message.channel.send('Gonk!')
+            .catch(console.error);
     }
 });
 
